5b335b1832bd22f92f5035df430d6150b527313e
Sam Saccone <samccone@google.com>
2016-08-24 21:09:40 -0700

    Finish reporter :)

--- gen_report.js
+++ gen_report.js
@@ -2,29 +2,23 @@ var path = require('path');
 var fs = require('fs');
 var MemoryStore = require('./lib/store/memory');
 var HtmlReporter = require('./lib/report/html');
+var Collector = require('./lib/collector');
 
 var dump = JSON.parse(fs.readFileSync(path.resolve(__dirname, process.argv[2]), 'utf8'));
 var originals = JSON.parse(dump.originals)
 var srcStore = new MemoryStore();
+var collector = new Collector();
+
+collector.add(JSON.parse(dump.coverage));
 
 Object.keys(originals).forEach(function(key) {
   srcStore.set(key, new Buffer(originals[key], 'base64').toString());
 });
 
-// TODO https://github.com/gotwarlost/istanbul/blob/master/lib/collector.js
 
-HtmlReporter({
-  sourceStore: srcStore
-})
+var reporter = new HtmlReporter({
+  sourceStore: srcStore,
+  collector: collector,
+});
 
-//sourceStore = Store.create('memory');
-
-
-
-
-/*reporter_instances = HtmlReporter({
-  sourceStore:
-})
-*/
-
-
+reporter.writeReport(collector, true);
